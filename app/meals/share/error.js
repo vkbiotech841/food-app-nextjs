f0469ced6df6358036c3792fd714166098b032a8
vikram <vkbiotech841@gmail.com>
2025-01-02 11:32:22 +0530

    metadata added

---
+++ app/meals/share/error.js
@@ -0,0 +1,11 @@
+'use client';
+
+// Note: Error page will be a client component because next js makes sure error can occure eight at the server side or from the client side and both need to be detactable. 
+
+
+export default function Error() {
+    return <main className="error">
+        <h1>An error occured!</h1>
+        <p>Failed to create meal.</p>
+    </main>
+}
--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,9 +1,24 @@
+
+'use client';
+
 import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
 import { shareMeal } from '@/lib/actions';
+import MealsFormSubmit from '@/components/meals/meals-form-submit';
+
+// import { useActionState } from "react";
+
+// Note: useFormState is deprecated in newer version of react. So, once you update the react version. Please useActionState instead of useFormState.
+
+
+
+import { useFormState } from 'react-dom';
+
 
 export default function ShareMealPage() {
+    // const [state, formAction] = useActionState(shareMeal, { message: null });
 
+    const [state, formAction] = useFormState(shareMeal, { message: null });
 
     return (
         <>
@@ -14,7 +29,7 @@ export default function ShareMealPage() {
                 <p>Or any other meal you feel needs sharing!</p>
             </header>
             <main className={classes.main}>
-                <form className={classes.form} action={shareMeal}>
+                <form className={classes.form} action={formAction}>
                     <div className={classes.row}>
                         <p>
                             <label htmlFor="name">Your name</label>
@@ -43,8 +58,9 @@ export default function ShareMealPage() {
                         ></textarea>
                     </p>
                     <ImagePicker label="Your image" name="image" />
+                    {state.message && <p>{state.message}</p>}
                     <p className={classes.actions}>
-                        <button type="submit">Share Meal</button>
+                        <MealsFormSubmit />
                     </p>
                 </form>
             </main>
